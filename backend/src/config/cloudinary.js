e0cfe003c175fab43951bc90f33df8511b631122
Juan Diego <guzaldjuan.004@gmail.com>
2025-12-04 10:04:07 -0600

    Funcion de subir imagenes y propuesta de diseño

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "TU_CLOUD_NAME",
-  api_key: "TU_API_KEY",
-  api_secret: "TU_API_SECRET"
+  cloud_name: "duiwgaaxh",
+  api_key: "765373594619282",
+  api_secret: "**********"
 });
 
 module.exports = cloudinary;
